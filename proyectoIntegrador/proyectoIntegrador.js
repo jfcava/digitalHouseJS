f046789f48a2eb8259c7473549a78eb03435cd01
Juan Cavallieri <juancavallieri@gmail.com>
2024-08-22 17:15:39 -0300

    Finalizacion proyecto integrador

--- proyectoIntegrador/proyectoIntegrador.js
+++ proyectoIntegrador/proyectoIntegrador.js
@@ -143,6 +143,27 @@ function ordenarTareasPorFechaLimite(){
 }
 
 
+// Funcion que busca una tarea por nombre y retorna su posicion
+function buscarTareaPorNombre(nombreTarea){
+    let inicio = 0;
+    let fin = tareas.length - 1;
+
+    while(inicio <= fin){
+        let posElementoMedio = Math.round((inicio + fin) / 2);
+        let elementoMedio = tareas[posElementoMedio];
+        
+        if(elementoMedio.nombre === nombreTarea){
+            return posElementoMedio;
+        }else if(elementoMedio.nombre < nombreTarea){
+            inicio = posElementoMedio + 1;
+        }else{
+            fin = posElementoMedio - 1;
+        }
+    }
+    return -1;
+}
+
+
 
 // Funcion para mostrar el menu de opciones
 
@@ -160,6 +181,7 @@ function mostrarMenu(){
     console.log("10. Visualizar todas las tareas no completadas");
     console.log("11. Ordenar las tareas alfabeticamente");
     console.log("12. Ordenar las tareas por fecha limite");
+    console.log("13. Buscar una tarea por su nombre");
     console.log("0. Salir");
 }
 
@@ -265,6 +287,19 @@ function interactuarConUsuario(){
                 ordenarTareasPorFechaLimite();
                 console.log("Tareas por fecha: ");
                 console.log(tareas);
+                break;
+
+            case 13:
+                ordenarTareasPorNombre();
+
+                let nombreABuscar = prompt("Ingrese nombre de la tarea a buscar: ");
+                let indiceTareaBuscada = buscarTareaPorNombre(nombreABuscar);
+
+                if(indiceTareaBuscada !== -1){
+                    console.log("Tarea encontrada en el indice: " + indiceTareaBuscada);
+                }else{
+                    console.log("Tarea no encontrada");
+                }
                 break;
             
             default:
